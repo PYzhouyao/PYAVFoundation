a4de4ba1783479e980027b711be668c3d7f13bf4
PYzhouyao <zhouyao@Mac-mini.local>
2016-02-17 11:18:01 +0800

    拍照功能

--- PYAVFoundation/ViewController.m
+++ PYAVFoundation/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+typedef void (^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
+
 @interface ViewController ()
 
 @end
@@ -23,26 +25,151 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self.captureSession startRunning];
+}
 
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    [self.captureSession stopRunning];
+}
+
+-(void)dealloc{
+    [self removeNotification];
+}
 
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     //初始化会话
     _captureSession = [[AVCaptureSession alloc]init];
-    if ([_captureSession canSetSessionPreset:AVCaptureSessionPreset1280x720]) {
+    if ([_captureSession canSetSessionPreset:AVCaptureSessionPreset1280x720]) {//设置分辨率
         _captureSession.sessionPreset = AVCaptureSessionPreset1280x720;
     }
     //获得输入设备
-    AVCaptureDevice *captureDevice = [self getCameraDeviceWithPosition:AVCaptureDevicePositionBack];
+    AVCaptureDevice *captureDevice = [self getCameraDeviceWithPosition:AVCaptureDevicePositionBack];//取得后置摄像头
     if (!captureDevice) {
         NSLog(@"取得后置摄像头时出现问题.");
         return;
     }
+    
+    NSError *error = nil;
+    //根据输入设备初始化设备输入对象，用于获得输入数据
+    _captureDeviceInput = [[AVCaptureDeviceInput alloc]initWithDevice:captureDevice error:&error];
+    if (error) {
+        NSLog(@"取得设备输入对象时出错，错误原因：%@",error.localizedDescription);
+        return;
+    }
+    //初始化设备输出对象，用于获得输出数据
+    _captureStillImageOutput = [[AVCaptureStillImageOutput alloc]init];
+    NSDictionary *outputSettings = @{AVVideoCodecKey:AVVideoCodecJPEG};
+    [_captureStillImageOutput setOutputSettings:outputSettings];
+    
+    //将设备输入添加到会话中
+    if ([_captureSession canAddInput:_captureDeviceInput]) {
+        [_captureSession addInput:_captureDeviceInput];
+    }
+    //将设备输出添加到会话中
+    if ([_captureSession canAddOutput:_captureStillImageOutput]) {
+        [_captureSession addOutput:_captureStillImageOutput];
+    }
+    
+    //创建视频预览层，用于实时展示摄像头状态
+    _captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc]initWithSession:_captureSession];
+    
+    CALayer *layer = self.viewContainer.layer;
+    layer.masksToBounds = YES;
+    
+    _captureVideoPreviewLayer.frame = layer.bounds;
+    _captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;//填充模式
+    //将视频预览层添加到界面中
+    [layer insertSublayer:_captureVideoPreviewLayer below:self.focusCursor.layer];
+    
+    [self addNotificationToCaptureDevice:captureDevice];
+    [self addGenstureRecognizer];
+    [self setFlashModeButtonStatus];
 }
 
+#pragma mark - UI方法
+#pragma mark 拍照
+- (IBAction)takeButtonClick:(UIButton *)sender {
+    //根据设备输出获得连接
+    AVCaptureConnection *captureConnection = [self.captureStillImageOutput connectionWithMediaType:AVMediaTypeVideo];
+    //根据连接取得设备输出的数据
+    [self.captureStillImageOutput captureStillImageAsynchronouslyFromConnection:captureConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+        if (imageDataSampleBuffer) {
+            NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+            UIImage *image = [UIImage imageWithData:imageData];
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+        }
+    }];
+    
+    
+}
+#pragma mark - 通知
+/**
+ *  给输入设备添加通知
+ */
+-(void)addNotificationToCaptureDevice:(AVCaptureDevice *)captureDevice{
+    //注意添加区域改变捕获通知必须首先设置设备允许捕获
+    [self changeDeviceProperty:^(AVCaptureDevice *captureDevice) {
+        captureDevice.subjectAreaChangeMonitoringEnabled = YES;
+    }];
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    //捕获区域发生改变
+    [notificationCenter addObserver:self selector:@selector(areaChange:) name:AVCaptureDeviceSubjectAreaDidChangeNotification object:captureDevice];
+}
+-(void)removeNotificationFromCaptureDevice:(AVCaptureDevice *)captureDevice{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self name:AVCaptureDeviceSubjectAreaDidChangeNotification object:captureDevice];
+}
+/**
+ *  移除所有通知
+ */
+-(void)removeNotification{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self];
+}
+-(void)addNotificationToCaptureSession:(AVCaptureSession *)captureSession{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    //会话出错
+    [notificationCenter addObserver:self selector:@selector(sessionRuntimeError:) name:AVCaptureSessionRuntimeErrorNotification object:captureSession];
+}
+
+/**
+ *  设备连接成功
+ *
+ *  @param notification 通知对象
+ */
+-(void)deviceConnected:(NSNotification *)notification{
+    NSLog(@"设备已连接...");
+}
+/**
+ *  设备连接断开
+ *
+ *  @param notification 通知对象
+ */
+-(void)deviceDisconnected:(NSNotification *)notification{
+    NSLog(@"设备已断开.");
+}
+/**
+ *  捕获区域改变
+ *
+ *  @param notification 通知对象
+ */
+-(void)areaChange:(NSNotification *)notification{
+    NSLog(@"捕获区域改变...");
+}
+/**
+ *  会话出错
+ *
+ *  @param notification 通知对象
+ */
+-(void)sessionRuntimeError:(NSNotification *)notification{
+    NSLog(@"会话发生错误.");
+}
 
 #pragma mark - 私有方法
-
 /**
  *  取得指定位置的摄像头
  *
@@ -59,5 +186,36 @@
         }
     }
     return nil;
+}
+
+/**
+ *  改变设备属性的统一操作方法
+ *
+ *  @param propertyChange 属性改变操作
+ */
+-(void)changeDeviceProperty:(PropertyChangeBlock)propertyChange{
+    AVCaptureDevice *captureDevice = [self.captureDeviceInput device];
+    NSError *error;
+    //注意改变设备属性前一定要首先调用lockForConfiguration:调用完之后使用unlockForConfiguration方法解锁
+    if ([captureDevice lockForConfiguration:&error]) {
+        PropertyChangeBlock(captureDevice);
+        [captureDevice unlockForConfiguration];
+    }else{
+        NSLog(@"设置设备属性过程发生错误，错误信息：%@",error.localizedDescription);
+    }
+}
+
+/**
+ *  添加点按手势，点按时聚焦
+ */
+-(void)addGenstureRecognizer{
+
+}
+/**
+ *  设置闪光灯按钮状态
+ */
+-(void)setFlashModeButtonStatus{
+
+    
 }
 @end
